bb0b098cd3178ee9d2fd1f0c4c8e9a7e7a377350
THIZY Kevin <Nakasar@users.noreply.github.com>
2017-10-03 16:28:35 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var express = require('express'),
 
 // mongoose connection
 mongoose.Promise = global.Promise;
-mongoose.connect('<DBURL>');
+mongoose.connect('mongodb://Abaddon:VNCm1Qo7PAzjPo57@gw2tools-shard-00-00-ozshx.mongodb.net:27017,gw2tools-shard-00-01-ozshx.mongodb.net:27017,gw2tools-shard-00-02-ozshx.mongodb.net:27017/test?ssl=true&replicaSet=GW2Tools-shard-0&authSource=admin');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
