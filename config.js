3d679cdb5be03700892daae16c7d504d063a7c9a
THIZY Kevin <Nakasar@users.noreply.github.com>
2017-10-24 14:03:42 +0200

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  'secret': 'a51E59d]@azd',
-  //'database': 'mongodb://localhost/gw2rp_api'
-  'database': 'mongodb://Abaddon:VNCm1Qo7PAzjPo57@gw2tools-shard-00-00-ozshx.mongodb.net:27017,gw2tools-shard-00-01-ozshx.mongodb.net:27017,gw2tools-shard-00-02-ozshx.mongodb.net:27017/test?ssl=true&replicaSet=GW2Tools-shard-0&authSource=admin'
-};
